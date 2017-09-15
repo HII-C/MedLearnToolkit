c353b3551665f0e5357716337f5d6060e9f1e734
amichne <austinmichne@austins-mbp.mobile.asu.edu>
2017-09-15 16:00:33 -0700

    Deidentify codes in patient record

--- function.py
+++ function.py
@@ -3,14 +3,22 @@ import psycopg2
 
 def patientToVector():
     patient_matrix = {}
+    code_dict = {}
+    code_count = 0
     conn = psycopg2.connect("dbname='mimic' user='student' host='localhost' password='password'")
     cur = conn.cursor()
     cur.execute("SELECT * from mimiciii.DIAGNOSES_ICD limit 100;")
     rows = cur.fetchall()
     for row in rows:
+        # row[1] = patient_id, row[2] = visit_id, row[4] = icd_9_code
         if row[1] in patient_matrix.keys():
             if row[2] in patient_matrix[row[1]].keys():
-                patient_matrix[row[1]][row[2]].append(row[4])
+                if (row[4] in code_dict.keys()):
+                    patient_matrix[row[1]][row[2]].append(code_dict[row[4]])
+                else:
+                    code_count += 1
+                    code_dict[row[4]] = code_count
+                    patient_matrix[row[1]][row[2]].append(code_dict[row[4]])
             else:
                 patient_matrix[row[1]][row[2]] = [row[4]]
         else:
