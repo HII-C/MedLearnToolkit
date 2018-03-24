ead9d89a32f438e5994abdd8a7d01724758f47e9
amichne <austinmichne@gmail.com>
2018-03-23 23:53:18 -0700

    Patients in mimic to single derived table

--- MimicToUmlsCui.py
+++ MimicToUmlsCui.py
@@ -10,6 +10,8 @@ class MimicToUmlsCui:
         self.umls_conn = None
         self.umls_cur = None
         self.umls_table = None
+        self.der_conn = None
+        self.der_cur = None
 
     def connect_gen_db(self, database):
         self.gen_conn = sql.connect(**database)
@@ -23,6 +25,10 @@ class MimicToUmlsCui:
         self.umls_conn = sql.connect(**database)
         self.umls_cur = self.umls_conn.cursor()
         self.umls_table = "MRCONSO"
+
+    def connect_der_db(self, database):
+        self.der_conn = sql.connect(**database)
+        self.der_cur = self.der_conn.cursor()
 
     def create_derived(self, tbl, n=10000):
         #self.mimic_cur.execute(f"DROP TABLE IF EXISTS {tbl}")
@@ -52,20 +58,47 @@ class MimicToUmlsCui:
         exec_str = f"CREATE TABLE derived.{tbl} AS {sel_str} from {j_str} where {l_str} limit 50" #temp
         self.mimic_cur.execute(exec_str)
         self.mimic_conn.commit()
+    
+    def mimic_table_to_umls_cui(self, source, umls_map, table, drop=False, n=1000):
+        source_to_int = {"DIAGNOSES_ICD": 0, "LABEVENTS": 1}
+        if drop == True:
+            self.der_cur.execute(f"DROP TABLE IF EXISTS {table}")
+            tbl_attr = "(SUBJECT_ID INT, HADM_ID INT, CUI CHAR(8), SOURCE SMALLINT UNSIGNED)"
+            self.der_cur.execute(f"""CREATE TABLE derived.{table} {tbl_attr}""")
+        insert_str = f"(SUBJECT_ID, HADM_ID, CUI, {source_to_int[source]})"
+        j_str = f"""
+            mimic.{source} m 
+                INNER JOIN 
+            derived.{umls_map} d
+                ON
+            d.ITEMID = m.ITEMID
+            """
+        exec_str = f"""
+            INSERT INTO derived.{table} {insert_str} 
+                SELECT m.SUBJECT_ID, m.HADM_ID, m.CUI
+            FROM {j_str} LIMIT {n}
+            """
+        self.der_cur.execute(exec_str)
 
 if __name__ == "__main__":
     print("Starting")
     # user = input("What is the user to access the databases?\n")
-    #user = 'root'
-    user = 'hiic' #temp measure for access
+    user = 'root'
+    # user = 'hiic' #temp measure for access
     pw = getpass(f'What is the password for the user {user}?\n')
 
     gen_db = {'user': user, 'host': 'db01.healthcreek.org', 'password': pw}
     mimic_db = {'user': user, 'db': 'mimic', 'host': 'db01.healthcreek.org', 'password': pw}
     umls_db = {'user': user, 'db': 'umls', 'host': 'db01.healthcreek.org', 'password': pw}
+    der_db = {'user': user, 'db': 'derived', 'host': 'db01.healthcreek.org', 'password': pw}
 
     example = MimicToUmlsCui()
     example.connect_gen_db(gen_db)
     example.connect_mimic_db(mimic_db)
     example.connect_umls_db(umls_db)
-    example.create_derived('ItemIdToCUI')
+    example.connect_der_db(der_db)
+    # example.create_derived('ItemIdToCUI')
+    example.mimic_table_to_umls_cui("DIAGNOSES_ICD",
+                                    "ItemIdToCUI", 
+                                    "patients_as_cui", 
+                                    drop=True)
