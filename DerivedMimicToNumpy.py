0f86f04e8fa941d4cc327ebd3196f98920b45063
amichne <austinmichne@gmail.com>
2018-03-25 01:45:26 -0700

    Beginning of GradBoost w/ sklearn, testing feature importance rankings

--- DerivedMimicToNumpy.py
+++ DerivedMimicToNumpy.py
@@ -1,5 +1,9 @@
 import MySQLdb as sql
 from collections import defaultdict
+from getpass import getpass
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+import xgboost as xg
 import numpy as np
 
 class DerivedMimicToNumpy:
@@ -19,21 +23,21 @@ class DerivedMimicToNumpy:
         return([*self.der_mimic_cur.fetchall()])
 
     def get_entries_for_paitents(self, patients, data_types=["Condition", "Observation", "Medication"]):
-        m_ = {"Condition": 1, "Observation": 0, "Medication": 2}
+        data__map = {"Observation": 0, "Condition": 1, "Medication": 2}
         dict_of_nparr = dict()
         if type(data_types) is str:
             data_types = [data_types]
-        for ty in data_types:
+        for data_type_ in data_types:
             set_of_codes = set()
             code_by_patient = defaultdict(list)
             for pat in patients:
                 self.der_mimic_cur.execute(
-                    f"SELECT CODE FROM {self.der_mimic_table} WHERE SUBJECT_ID = {pat} and SOURCE = {m_[ty]}")
+                    f"SELECT CODE FROM {self.der_mimic_table} WHERE SUBJECT_ID = {pat} and SOURCE = {data__map[data_type_]}")
                 tmp = [*self.der_mimic_cur.fetchall()]
                 code_by_patient[pat] = tmp
                 set_of_codes = set(tmp) | set_of_codes
 
-            self.universe_of_codes[ty] = set_of_codes
+            self.universe_of_codes[data_type_] = set_of_codes
             base_d = dict()
             # Use X = codes, Y = patients, set dtype to smallest int since binary but int needed for ML
             full_np_arr = np.ndarray(shape=(len(set_of_codes), len(patients)), dtype=np.int8)
@@ -46,8 +50,26 @@ class DerivedMimicToNumpy:
                     pat_d[key] = 1
                 for index, val in enumerate([*pat_d.values()]):
                     full_np_arr[index][pat_index] = val
-            dict_of_nparr[ty] = full_np_arr
+            dict_of_nparr[data_type_] = full_np_arr
         return dict_of_nparr
 
+    def init_ML_model(self, data, target, target_dtype):
+        labels = list()
+        target_col = list(self.universe_of_codes[target_dtype]).index(target)
+        for bin_exist in target[target_col]:
+            labels.append(bin_exist)
+        X_train, x_test, Y_train, y_test = train_test_split(data, labels)
+        # xg_train = xg.DMatrix(x_train, y_train)
+        # regr = xg.XGBClassifier(objective="binary:logistic")
+        regr = GradientBoostingClassifier()
+        regr.fit(X_train, Y_train)
+        print(regr.feature_importances_[0:10])
+
 if __name__ == "__main__":
-    
+    example = DerivedMimicToNumpy()
+    user = 'root'
+    pw = getpass(f"What is the password for the user {user}\n")
+    der_db = {'user': user, 'db': 'derived', 'host': 'db01.healthcreek.org', 'password': pw}
+    example.connect_der_mimic_db(der_db, "patients_as_cui")
+    example_patient_id_arr = example.get_patients()
+    example.get_entries_for_paitents(example_patient_id_arr, data_types=["Condition", "Observation"])
