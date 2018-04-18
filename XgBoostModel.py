40274c6707506bd41bceecc8e0d20d845adfc70b
amichne <austinmichne@gmail.com>
2018-04-18 10:44:54 -0700

    Fixing bug with data

--- XgBoostModel.py
+++ XgBoostModel.py
@@ -55,26 +55,30 @@ class XgBoostModel:
                     SOURCE = {data_map[self.lhs_type]}"""
         self.der_mimic_cur.execute(exec_str)
         entries = self.der_mimic_cur.fetchall()
-        if (self.lhs_type == self.rhs_type) and (self.target != None):
-            self.universe_of_codes[self.lhs_type] = set([x[1] for x in entries])
-            try:
-                self.universe_of_codes[self.lhs_type].remove(self.target)
-            except:
-                raise ValueError(f"Target defined ({self.target}) not in universe of codes, impossible to model.")
-        elif (self.lhs_type == self.rhs_type) and (self.target is None):
-            raise ValueError(f"Target of class must be defined for matching LHS and RHS types (currently {self.rhs_type})")
-        else:
-            self.universe_of_codes[self.lhs_type] = set([x[1] for x in entries])
+        # TODO: Below commented section has a bug, but this section should allow
+        # mapping from condition->condition, obs->obs, etc
+        # if (self.lhs_type == self.rhs_type) and (self.target != None):
+        #     self.universe_of_codes[self.lhs_type] = set([x[1] for x in entries])
+        #     try:
+        #         self.universe_of_codes[self.lhs_type].remove(self.target)
+        #     except:
+        #         raise ValueError(f"Target defined ({self.target}) not in universe of codes, impossible to model.")
+        # elif (self.lhs_type == self.rhs_type) and (self.target is None):
+        #     raise ValueError(f"Target of class must be defined for matching LHS and RHS types (currently {self.rhs_type})")
+        # else:
+        #     self.universe_of_codes[self.lhs_type] = set([x[1] for x in entries])
+        # for entry in entries:
+        #     if (self.lhs_type == self.rhs_type):
+        #         if (entry[1] != self.target):
+        #             entry_dict[self.lhs_type][entry[0]].append(entry[1])
+        #         else: continue
+        #     else:
+        #         entry_dict[self.lhs_type][entry[0]].append(entry[1])       
 
+        self.universe_of_codes[self.lhs_type] = set([x[1] for x in entries])
         for entry in entries:
-            if (self.lhs_type == self.rhs_type):
-                if (entry[1] != self.target):
-                    entry_dict[self.lhs_type][entry[0]].append(entry[1])
-                else: continue
-            else:
-                entry_dict[self.lhs_type][entry[0]].append(entry[1])       
-        print(self.lhs_type)
-        print(list(self.universe_of_codes.keys()))
+            entry_dict[self.lhs_type][entry[0]].append(entry[1])
+
         # Use X = codes, Y = patients, set dtype to smallest int since binary but int needed for ML
         full_np_arr = np.ndarray(shape=(len(patients),len(self.universe_of_codes[self.lhs_type])), dtype=np.int8)
         base_d = dict()
@@ -93,9 +97,6 @@ class XgBoostModel:
     
     def get_RHS_for_entry_matrix(self, patients, target=None):
         data_map = {"Condition": 0, "Observation": 1, "Medication": 2}
-        print(f"Forming RHS of type {self.rhs_type} now.")
-        if target is not None:
-            self.target = target
         labels = defaultdict(lambda: 0)
         # target_col = list(self.universe_of_codes[target_type]).index(target)
         exec_str = f"""
@@ -129,11 +130,20 @@ class XgBoostModel:
     def init_xg_gtb(self, lhs_matrix, rhs_matrix):
         split_size = .1
         self.X_train, self.x_test, self.Y_train, self.y_test = train_test_split(lhs_matrix, rhs_matrix, test_size=split_size)
+        # d_train = xg.DMatrix(self.X_train, self.Y_train, feature_names=list(self.universe_of_codes[self.lhs_type]))
+        # param = {'max_depth':7, 'eta':.2, 'objective':'binary:logistic'}
+        # num_round = 4
+        # self.model = xg.train(param, d_train, num_round)
+        # /////////////////////////////////////////////////////////////////////////////
+        regr = xg.XGBClassifier(objective="binary:logistic")
+        regr.fit(self.X_train, self.Y_train)
+        print(regr.feature_importances_)
+        y_pred = regr.predict(self.x_test)
+        preds = [round(value) for value in y_pred]
+        accuracy = accuracy_score(self.y_test, preds)
         print(f"Data split into {split_size} train:test. Creating model now.")
-        d_train = xg.DMatrix(self.X_train, self.Y_train, feature_names=list(self.universe_of_codes[self.lhs_type]))
-        param = {'max_depth':7, 'eta':.2, 'objective':'binary:logistic'}
-        num_round = 4
-        self.model = xg.train(param, d_train, num_round)
+        print(f"Accuracy in {len(self.x_test)} test cases = {accuracy * 100.0}")
+        # /////////////////////////////////////////////////////////////////////////////
         print("Model creation is finished.")
     
     def prediction_acc(self):
@@ -150,12 +160,12 @@ class XgBoostModel:
 
 
 if __name__ == "__main__":
-    example = XgBoostModel("Observation", "Condition", "C0375113")
+    example = XgBoostModel("Condition", "Observation", "C0364968")
     user = 'root'
     pw = getpass(f"Enter password for the user \'{user}\': ")
     der_db = {'user': user, 'db': 'derived', 'host': 'db01.healthcreek.org', 'password': pw}
     example.connect_der_mimic_db(der_db, "patients_as_cui")
-    example_patient_id_arr = example.get_patients(n=10000)
+    example_patient_id_arr = example.get_patients(n=20000)
     lhs_returned = example.get_LHS_for_entry_matrix(example_patient_id_arr)
     condition_labels_for_target = example.get_RHS_for_entry_matrix(example_patient_id_arr)
     example.init_xg_gtb(lhs_returned, condition_labels_for_target)
