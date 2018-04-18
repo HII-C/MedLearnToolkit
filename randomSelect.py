dcc0f6c9051100d0429fb0ee5f7ba95e8b454601
jamesyu5421 <student@general.healthcreek.org>
2018-04-17 20:17:57 -0700

    code will randomly select from table with ROW_ID column

--- randomSelect.py
+++ randomSelect.py
@@ -15,13 +15,14 @@ class randomSelect(object):
 
     # gets random results and stores in table rands in derived database
     def select_random_rows(self, accesstable, requestedColumns, randNum):
+        exec_str = f"DROP PROCEDURE IF EXISTS get_rands;"
+        self.der_cur.execute(exec_str) 
         exec_str = f'''
                         
-                        DROP PROCEDURE IF EXISTS get_rands;
                         CREATE PROCEDURE get_rands(IN cnt INT)
                         BEGIN
-                        DROP TEMPORARY TABLE IF EXISTS rands;
-                        CREATE TEMPORARY TABLE rands ( rand_id INT );
+                        DROP TABLE IF EXISTS rands;
+                        CREATE TABLE rands ( rand_id INT );
 
                         loop_me: LOOP
                             IF cnt < 1 THEN
@@ -29,24 +30,22 @@ class randomSelect(object):
                             END IF;
 
                             INSERT INTO rands
-                            SELECT {requestedColumns}
+                            SELECT r1.ROW_ID
                             FROM {accesstable} AS r1 JOIN
                                 (SELECT (RAND() *
-                                (SELECT MAX(id)
+                                (SELECT MAX(ROW_ID)
                                     FROM {accesstable})) AS id)
                                         AS r2
-                                WHERE r1.id >= r2.id
-                                ORDER BY r1.id ASC
+                                WHERE r1.ROW_ID >= r2.id
+                                ORDER BY r1.ROW_ID ASC
                                 LIMIT 1;
 
                             SET cnt = cnt - 1;
                             END LOOP loop_me;
-                        END$$
-                        
-                        
-                        CALL get_rands({randNum});
-        
-                 '''
+                        END; '''
+        self.der_cur.execute(exec_str)
+        self.der_conn.commit()
+        exec_str = f" CALL get_rands({randNum});"
         self.der_cur.execute(exec_str)
         self.der_conn.commit()
 
@@ -62,7 +61,7 @@ if __name__ == "__main__":
     example.connect_der_db(der_db)
 
     # requested columns should be in form r1.[column], table name should be in form [database].[table]
-    example.select_random_rows("","" , 10)
+    example.select_random_rows('mimic.D_LABITEMS','r1.CUI' , 10)
 
 
         
